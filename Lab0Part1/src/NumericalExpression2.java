b532656b93375afc4d5beceb02b3996d18e6a9eb
Phuong Tran <tran28@my.yorku.ca>
2019-01-09 13:14:40 -0500

    Java Tutorial 1-3
    Completed example classes in tutorial 1-3.

---
+++ Lab0Part1/src/NumericalExpression2.java
@@ -0,0 +1,36 @@
+
+public class NumericalExpression2 {
+
+	public static void main(String[] args) {
+	
+		// 15 + 4 this is a comment that is not executed by the computer
+		System.out.println("15 + 4: " + (15 + 4)); // 19
+		// 15 - 4
+		System.out.println("15 - 4: " + (15 - 4)); // 11
+		// 15 * 4
+		System.out.println("15 * 4: " + (15 * 4)); // 60
+		// 15 / 4 because both are integers, we will get the quotient 
+		System.out.println("15 / 4 gives the quotient: " + (15 / 4)); // 3
+		// 15 % 4
+		System.out.println("15 % 4 gives the remainder: " + (15 % 4)); // 3
+	
+		// 23 / 4 because both are integers, we will get the quotient 
+		System.out.println("23 / 4 gives the quotient: " + (23 / 4)); // 5
+		// 23 % 4
+		System.out.println("23 % 4 gives the remainder: " + (23 % 4)); // 3
+				
+		// 4 * (23 / 4) + (23 % 4) = 23
+		System.out.println("divisor * quotient + remainder = original: " + (4 * (23 / 4) + (23 % 4))); // 23
+				
+		// System.out.println("1" * "1"); // this is not correct
+		System.out.println("output 1 + 1 verbatim: " + "1 + 1");
+		System.out.println("output the result of calculating 1 + 1: " + (1 + 1));
+				
+		// 5.2 / 2
+		System.out.println(5.2 / 2); // 2.6
+		// 5 / 2.5
+		System.out.println(5 / 2.5); // 2.0
+
+	}
+
+}
---
+++ Lab0Part1/src/SimpleConsoleOutput.java
@@ -0,0 +1,12 @@
+
+public class SimpleConsoleOutput {
+
+	// main method is entry point of runtime execution
+	public static void main(String[] args) {
+		
+		System.out.println("Hello EECS1021!");
+		System.out.println("Now is Winter 2019");
+
+	}
+
+}
